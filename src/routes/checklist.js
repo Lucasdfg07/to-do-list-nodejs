1fb0cd76c90d24cdf2cbb01e7d690bcca05a7221
Lucas Siqueira Fernandes <lucassiqueirafernandes@MacBook-Air-de-Lucas.local>
2020-06-11 23:21:12 -0300

    Setting models and http requests

--- src/routes/checklist.js
+++ src/routes/checklist.js
@@ -2,24 +2,56 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send();
+const Checklist = require('../models/checklist');
+
+router.get('/', async (req, res) => {
+    try {
+        let checklists = await Checklist.find({});
+        res.status(200).json(checklists);
+    } catch(e) {
+        res.status(500).json(e);
+    }
 })
 
-router.post('/', (req, res) => {
-    res.status(200).json(req.body);
+router.post('/', async (req, res) => {
+    let { name } = req.body;
+
+    try {
+        let checklist = await Checklist.create({name});
+        res.status(200).json(checklist);
+    } catch(e) {
+        res.status(422).json(e);
+    }
+
 })
 
-router.get('/:id', (req, res) => {
-    res.send(`ID: ${req.params.id}`);
+router.get('/:id', async (req, res) => {
+    try {
+        let checklist = await Checklist.findById(req.params.id);
+        res.status(200).json(checklist);
+    } catch(e) {
+        res.status(422).json(e);
+    }
 })
 
-router.put('/:id', (req, res) => {
-    res.send(`PUT ID: ${req.params.id}`);
+router.put('/:id', async (req, res) => {
+    let { name } = req.body;
+
+    try {
+        let checklist = await Checklist.findByIdAndUpdate(req.params.id, {name}, {new: true});
+        res.status(200).json(checklist);
+    } catch(e) {
+        res.status(422).json(e);
+    }
 })
 
-router.delete('/:id', (req, res) => {
-    res.send(`DELETE ID: ${req.params.id}`);
+router.delete('/:id', async (req, res) => {
+    try {
+        let checklist = await Checklist.findByIdAndRemove(req.params.id);
+        res.status(200).json(checklist);
+    } catch(e) {
+        res.status(422).json(e);
+    }
 })
 
 module.exports = router;
