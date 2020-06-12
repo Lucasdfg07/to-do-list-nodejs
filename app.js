589ae8bfd0ca69421c78d3833167ceb5e9c04a44
Lucas Siqueira Fernandes <lucassiqueirafernandes@MacBook-Air-de-Lucas.local>
2020-06-12 00:06:23 -0300

    Setting style and view

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
-const checkListRouter = require('./src/routes/checklist') 
-require('./config/database')
+const checkListRouter = require('./src/routes/checklist'); 
+const rootRouter = require('./src/routes/index'); 
+require('./config/database');
+const path = require('path');
+
 
 const app = express();
-
 app.use(express.json());
 
+app.set('views', path.join(__dirname, 'src/views'));
+app.set('view engine', 'ejs');
+
+app.use('/', rootRouter);
 app.use('/checklists', checkListRouter);
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(3000, () => {
     console.log('Servidor Iniciado')
